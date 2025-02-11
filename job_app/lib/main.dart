3dac742f012baac5c43227c2fee43cc19249eddb
dmhchandrasiri <madushahc@gmail.com>
2025-02-11 20:37:02 +0530

    remove device preview

--- job_app/lib/main.dart
+++ job_app/lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() {
   runApp(
-    DevicePreview(
-      builder: (context) => MyApp(),
-    ),
+    MyApp(),
   );
 }
 
