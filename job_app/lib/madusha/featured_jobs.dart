0b74a7abf668b1c83b45a4453cbc659528b3becb
dmhchandrasiri <madushahc@gmail.com>
2025-02-22 21:48:41 +0530

    Create User Interface
    Card design, Image carousel

--- job_app/lib/madusha/featured_jobs.dart
+++ job_app/lib/madusha/featured_jobs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:job_app/home_page.dart';
+import 'package:job_app/madusha/customcard.dart';
 
 class FeaturedJobs extends StatelessWidget {
   final bool isDarkMode;
@@ -15,76 +16,50 @@ class FeaturedJobs extends StatelessWidget {
   Widget build(BuildContext context) {
     final Color textColor = isDarkMode ? Colors.white : Colors.black87;
 
-    return SafeArea(
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(
-              left: 16.0,
-              right: 16.0,
-            ),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Featured Jobs",
-                      style: TextStyle(
-                          color: textColor,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    GestureDetector(
-                      onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HomePage1(),
-                        ),
-                      ),
-                      child: Text(
-                        "see all",
-                        style: TextStyle(color: Colors.grey, fontSize: 16.0),
-                      ),
-                    )
-                  ],
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 10.0),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      spacing: 15.0,
-                      children: [
-                        Container(
-                          width: 250,
-                          height: 150,
-                          color: Colors.amberAccent,
-                        ),
-                        Container(
-                          width: 250,
-                          height: 150,
-                          color: const Color.fromARGB(255, 110, 105, 90),
-                        ),
-                        Container(
-                          width: 250,
-                          height: 150,
-                          color: const Color.fromARGB(255, 83, 64, 255),
-                        ),
-                        Container(
-                          width: 250,
-                          height: 150,
-                          color: const Color.fromARGB(255, 255, 64, 169),
-                        ),
-                      ],
-                    ),
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Featured Jobs",
+                style: TextStyle(
+                    color: textColor,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage1(),
                   ),
-                )
-              ],
-            ),
+                ),
+                child: Text(
+                  "see all",
+                  style: TextStyle(color: Colors.lightBlue, fontSize: 16.0),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        SizedBox(height: 10.0), // Reduced extra spacing
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.only(left: 15.0, bottom: 15.0),
+          child: Row(
+            spacing: 15.0,
+            children: [
+              CustomCard(),
+              CustomCard(),
+              CustomCard(),
+              CustomCard(),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
