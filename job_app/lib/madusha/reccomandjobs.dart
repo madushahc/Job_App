6667ac5fa46c7725269e636557901442ca551eed
dmhchandrasiri <madushahc@gmail.com>
2025-03-09 10:39:06 +0530

    Add Apply Button

--- job_app/lib/madusha/reccomandjobs.dart
+++ job_app/lib/madusha/reccomandjobs.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
 import 'package:job_app/madusha/jobdetailscreen.dart';
 import 'package:amicons/amicons.dart';
 import 'package:job_app/madusha/seeallrecomjobs.dart';
