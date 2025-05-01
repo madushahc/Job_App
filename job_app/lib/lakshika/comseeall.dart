b8cdaf587e4daba98f92791c83fac64c94f4ff62
madushahc <madushahc@gmail.com>
2025-05-01 18:30:06 +0530

    fetch companies data

--- job_app/lib/lakshika/comseeall.dart
+++ job_app/lib/lakshika/comseeall.dart
@@ -39,7 +39,7 @@ class _CompanySeeAllState extends State<CompanySeeAll> {
 
     final headers = {
       'x-rapidapi-host': 'jsearch.p.rapidapi.com',
-      'x-rapidapi-key': 'b82235208amsh8a43112a2c5c8e4p19ceb3jsn0ceb21592560',
+      'x-rapidapi-key': '48fea61a3fmsh72dc8d6f1b29208p1cd121jsn5b7f40a19052',
     };
 
     try {
@@ -132,7 +132,7 @@ class _CompanySeeAllState extends State<CompanySeeAll> {
                               child: Card(
                                 color: cardColor,
                                 elevation: 4.0,
-                                margin: EdgeInsets.only(right: 16.0, top: 12.0),
+                                margin: EdgeInsets.only(top: 12.0),
                                 shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(15.0),
                                 ),
