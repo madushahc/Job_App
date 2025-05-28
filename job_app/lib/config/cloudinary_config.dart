e242f4f5f3c416de51655eaf0129f97933b09d39
Sathila Thimalka <sathilathimalkad@gmail.com>
2025-05-28 10:12:31 +0530

    Update cloudinary_config.dart

--- job_app/lib/config/cloudinary_config.dart
+++ job_app/lib/config/cloudinary_config.dart
@@ -11,7 +11,7 @@ class CloudinaryConfig {
 class RapidApiConfig {
   static const String host = 'jsearch.p.rapidapi.com';
   static const String apiKey =
-      '48fea61a3fmsh72dc8d6f1b29208p1cd121jsn5b7f40a19052';
+      '45e6271b49msh02064a264dbc139p15c911jsnb36740740f5b';
 
   static Map<String, String> getHeaders() {
     return {
